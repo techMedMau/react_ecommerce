1ab5db7a2b2e28ef33c513f928fcaa8967962043
Maureen Chang <76696006+techMedMau@users.noreply.github.com>
2024-04-04 22:30:29 -0700

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Footer = ({ products }) => {
     return (
-        <div>Footer</div>
+        <footer className='bg-yellow-200 py-6'>
+            <div className='container mx-auto'>
+                <p className='text-slate-500 text-center'>Copyright &copy; Maureen Chang 2024. All rights reserved.</p>
+            </div>
+        </footer>
     );
 };
 
